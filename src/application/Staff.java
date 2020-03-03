442579ff4375e472a746f4adbed299da279dab24
FilipLiljebris <56776060+FilipLiljebris@users.noreply.github.com>
2020-03-03 10:22:51 +0100

    Staff class
    Created a base for the new class "Staff".

---
+++ src/application/Staff.java
@@ -0,0 +1,50 @@
+package application;
+
+public class Staff {
+	private String name;
+	private String socialNumber;
+	private String phoneNumber;
+	private String email;
+	private String title;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSocialNumber() {
+		return socialNumber;
+	}
+
+	public void setSocialNumber(String socialNumber) {
+		this.socialNumber = socialNumber;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+}
